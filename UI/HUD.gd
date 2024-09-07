cb7f213f60172c130ad34bbc2c7d81bd57d575a8
Szymon-Groch <szymon.groch20@gmail.com>
2024-09-07 21:03:14 +0200

    Addint tutorial instruction

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -2,6 +2,9 @@ extends Control
 
 onready var score_label = $ScoreLabel
 onready var lives_texture = $LivesTexture
+onready var tutorial_timer = $TutorialTimer
+onready var tutorial = $Tutorial
+
 
 func update_score(value):
 	score_label.text = "SCORE: " + str(value)
@@ -9,3 +12,7 @@ func update_score(value):
 func update_lives(val):
 	lives_texture.rect_size.x = val * 37
 	
+
+
+func _on_TutorialTimer_timeout() -> void:
+	tutorial.queue_free()
