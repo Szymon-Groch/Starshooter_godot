2b26dc36415095a3e98fee36711a529a2fea80ea
Szymon-Groch <szymon_groszek@wp.pl>
2024-09-07 14:14:34 +0200

    Creating player scence

---
+++ characters/player/Player.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export (int) var speed = 300
+
+var input_vector = Vector2.ZERO
+
+func _physics_process(delta):
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") # -1 0 1
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	global_position.x += input_vector.x * speed * delta 
+	global_position.y += input_vector.y * speed * delta 
