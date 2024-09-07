53709051dc2b9c4243c444f82adf653f451ca8f6
Szymon-Groch <szymon.groch20@gmail.com>
2024-09-07 14:22:55 +0200

    adding comment

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -5,8 +5,8 @@ export (int) var speed = 300
 var input_vector = Vector2.ZERO
 
 func _physics_process(delta):
-	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") # -1 0 1
-	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") # -1 0 1 | -1 + 1 =0
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up") # -1 0 1 | -1 + 1 =0
 	
 	global_position.x += input_vector.x * speed * delta 
 	global_position.y += input_vector.y * speed * delta 
